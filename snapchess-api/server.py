570019c5e2bade5d50dce8306c92ab72f3baabe6
Chao Ren <chowypatowy0428@gmail.com>
2025-07-06 20:42:41 +0100

    indentation fix

--- snapchess-api/server.py
+++ snapchess-api/server.py
@@ -21,6 +21,6 @@ def upload():
         return jsonify({"message": result})
     
 if __name__ == "__main__":
-port = int(os.environ.get("PORT", 10000))
+    port = int(os.environ.get("PORT", 10000))
     app.run(host="0.0.0.0", port=port)
 # ngrok http --host-header=rewrite localhost:4000
